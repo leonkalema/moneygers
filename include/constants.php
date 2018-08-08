b86692755407ca542466bb769272056aca87403b
leonkalema <tullapeople@gmail.com>
2018-08-08 11:16:16 +0200

    Update constants.php

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 	define("MYSQL_HOST", "localhost");
 	define ("MYSQL_USER", "root");
 	define ("MYSQL_PASSWORD", "");
-	define ("MYSQL_DATABASE", "cleaning");
+	define ("MYSQL_DATABASE", "db417523907");
 	define ("USER_NAME", "USER_NAME");
 	define ("SINGLE_COUNT", "365");
 	define ("UPLOAD_DIRECTORY", $_SERVER['DOCUMENT_ROOT']."/hr/uploads");
